e23aa60b753837f4cc50ee86fb64c329b242bdea
Abdullahi Mohamed <abmoh_8228@hotmail.com>
2016-03-06 15:32:59 +0100

    InitialCommit

---
+++ GithubTest/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GithubTest
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Im goint to demonstrate a addition program");
+
+            Console.WriteLine("choose first number");
+            int num1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("choose second number");
+            int num2 = int.Parse(Console.ReadLine());
+
+            int result = num1 + num2;
+
+            Console.WriteLine("The answer is {0}", result);
+
+            Console.ReadKey();
+        }
+    }
+}
